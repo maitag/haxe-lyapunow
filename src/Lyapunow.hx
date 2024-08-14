54378d631e37789efa0efe580291511f54e8c578
maitag <info@maitag.de>
2024-08-14 17:56:33 +0200

    shadergeneration

--- src/Lyapunow.hx
+++ src/Lyapunow.hx
@@ -1,7 +1,10 @@
 package;
 
 import peote.view.*;
+import Formula;
+
 import Param.DefaultParams;
+import Param.FormulaParams;
 
 class Lyapunow implements Element
 {
@@ -19,119 +22,143 @@ class Lyapunow implements Element
 	static public var buffer:Buffer<Lyapunow>;
 	static public var program:Program;	
 	
-	static public function init(display:Display, defaultParams:DefaultParams)
+	static public function init(display:Display, formula:Formula, sequence:Array<String>, defaultParams:DefaultParams, formulaParams:FormulaParams)
 	{	
 		buffer = new Buffer<Lyapunow>(1);
 		program = new Program(buffer);
 		
-		program.injectIntoFragmentShader(
-		"
-			float func(float i, float n)
-			{
-				return 2.5*sin(i+n)*sin(i+n)+2.0;
-				// return uParama*sin(i+n)*sin(i+n)+uParamb;
-
-				// return #FORMULA;
-			}
-
-			float deriv(float i, float n)
-			{
-				return 2.5*sin(2.0*(i+n));
-				// return uParama*sin(2.0*(i+n));
-
-				// return #DERIVATE;
-			}
-
-			void pre_step(inout float i, vec2 xy)
-			{
-				i = func(i,xy.x);
-				i = func(i,xy.y);
-
-				// #PRE_SQEUENCE_CALLS
-			}
-
-			void main_step(inout float index, inout float i, vec2 xy)
-			{
-				i = func(i,xy.x);
-				index += (  log(abs(deriv(i,xy.x)))*uBalance + deriv(i,xy.x)*(1.0-uBalance)  ) / 2.0;
-
-				i = func(i,xy.y);
-				index += (  log(abs(deriv(i,xy.y)))*uBalance + deriv(i,xy.y)*(1.0-uBalance)  ) / 2.0;
-				
-				// #PRE_SQEUENCE_CALLS
-			}
-
-			vec4 lyapunow()
-			{
-				vec2 uPosition = vec2(0.0, 0.0);
-				vec2 uScale = vec2(800.0/vSize.x, 800.0/vSize.y);
-				
-				vec3 uColpos = vec3(1.0, 0.0, 0.0);
-				vec3 uColmid = vec3(0.0, 0.0, 0.0);
-				vec3 uColneg = vec3(0.0, 0.0, 1.0);
-
-
-				// Parameter
-				float i = uStartIndex;
-				vec2 xy = (vTexCoord - uPosition) / uScale;
-				
-				int iter_pre =  int(floor(uIterPre));
-				int iter_main = int(floor(uIterMain));
-				float iter_main_full = floor(uIterMain) * 2.0; // todo, the 2.0 is generated in depend of how long the sequence is !
-				if (iter_main_full == 0.0) iter_main_full = 1.175494351e-38;
-
-				float nabla_pre = uIterPre - float(iter_pre);
-				float nabla_main = uIterMain - float(iter_main);
-				
-				float index = 0.0;
-				
-				// pre-iteration ##########################
-				
-				for (int iter = 0; iter < 21; iter++) {
-					if (iter < iter_pre)
-					{
-						pre_step(i, xy);
-					}
-				}
-				if (nabla_pre != 0.0) {
-					float x_pre = i;
-					pre_step(i, xy);
-					i = i*nabla_pre + x_pre*(1.0-nabla_pre);
-				}
-					
-				// main-iteration ########################
-				
-				for (int iter = 0; iter < 201; iter++) {
-					if (iter < iter_main)
-					{
-						main_step(index, i, xy);
-					}
-				}
-				
-				if (nabla_main == 0.0) {
-					index = index/iter_main_full;
-				}
-				else {
-					float index_pre = index/iter_main_full;
-
-					main_step(index, i, xy);
-
-					index = index/(iter_main_full + 2.0); // todo, the 2.0 is generated in depend of how long the sequence is !
-					index = index*nabla_main + index_pre*(1.0-nabla_main);
-				}
-
-				return vec4( index*( (index > 0.0) ? uColpos-uColmid : uColmid-uColneg   )+uColmid, 1.0 );
-			}			
-		"
-		, false // inject uTime
-		, defaultParams.uniforms
-		);
+		program.setColorFormula( 'lyapunow()', false );
+		program.setFragmentFloatPrecision("high", false);
+		updateShader(formula, sequence, defaultParams, formulaParams);
 		
-		program.setColorFormula( 'lyapunow()' );
 		display.addProgram(program);
 
 
         buffer.addElement( new Lyapunow() );
+	}
+
+	static public function updateShader(formula:Formula, sequence:Array<String>, defaultParams:DefaultParams, formulaParams:FormulaParams) {
+
+		// trace("formula", formula.toString("glsl"));
+
+		var derivate:Formula = formula.derivate("i").simplify();
+		// trace("derivate", derivate.toString("glsl"));
+
+		var pre_sequence:String = "";
+		var main_sequence:String = "";
+
+		var extra_param:String = "";
+		var extra_func_param:String = "";
+
+		if (formula.hasParam("x")) {extra_func_param += ",float x"; extra_param += ",xy.x";}
+		if (formula.hasParam("y")) {extra_func_param += ",float y"; extra_param += ",xy.y";}
+
+		for (s in sequence) {
+			var p:String = (s == "x" || s == "y") ? "xy."+s : "uParam"+s;
+			p += extra_param;
+			pre_sequence += 'i = func(i, $p);';
+			main_sequence +='i = func(i, $p); index += (log(abs(deriv(i, $p)))*uBalance + deriv(i, $p)*(1.0-uBalance)) / 2.0;';
+		}
+
+		program.injectIntoFragmentShader(
+			'
+				float func(float i, float n $extra_func_param)
+				{
+					// return 2.5*sin(i+n)*sin(i+n)+2.0;
+					// return uParama*sin(i+n)*sin(i+n)+uParamb;	
+					return ${formula.toString("glsl")};
+				}
+	
+				float deriv(float i, float n $extra_func_param)
+				{
+					// return 2.5*sin(2.0*(i+n));
+					// return uParama*sin(2.0*(i+n));	
+					return ${derivate.toString("glsl")};
+				}
+	
+				void pre_step(inout float i, vec2 xy)
+				{
+					// i = func(i, xy.x);
+					// i = func(i, xy.y);	
+					$pre_sequence
+				}
+	
+				void main_step(inout float index, inout float i, vec2 xy)
+				{
+					// i = func(i, xy.x);
+					// index += (  log(abs(deriv(i, xy.x)))*uBalance + deriv(i, xy.x)*(1.0-uBalance)  ) / 2.0;
+	
+					// i = func(i, xy.y);
+					// index += (  log(abs(deriv(i, xy.y)))*uBalance + deriv(i, xy.y)*(1.0-uBalance)  ) / 2.0;					
+					$main_sequence
+				}
+	
+				vec4 lyapunow()
+				{
+					vec2 uPosition = vec2(0.0, 0.0);
+					vec2 uScale = vec2(800.0/vSize.x, 800.0/vSize.y);
+					
+					vec3 uColpos = vec3(1.0, 0.0, 0.0);
+					vec3 uColmid = vec3(0.0, 0.0, 0.0);
+					vec3 uColneg = vec3(0.0, 0.0, 1.0);
+	
+	
+					// Parameter
+					float i = uStartIndex;
+					vec2 xy = (vTexCoord - uPosition) / uScale;
+					
+					int iter_pre =  int(floor(uIterPre));
+					int iter_main = int(floor(uIterMain));
+					float iter_main_full = floor(uIterMain) * ${sequence.length}.0; // in depend of how long the sequence is !
+					if (iter_main_full == 0.0) iter_main_full = 1.175494351e-38;
+	
+					float nabla_pre = uIterPre - float(iter_pre);
+					float nabla_main = uIterMain - float(iter_main);
+					
+					float index = 0.0;
+					
+					// pre-iteration ##########################
+					
+					for (int iter = 0; iter < 21; iter++) {
+						if (iter < iter_pre)
+						{
+							pre_step(i, xy);
+						}
+					}
+					if (nabla_pre != 0.0) {
+						float x_pre = i;
+						pre_step(i, xy);
+						i = i*nabla_pre + x_pre*(1.0-nabla_pre);
+					}
+						
+					// main-iteration ########################
+					
+					for (int iter = 0; iter < 201; iter++) {
+						if (iter < iter_main)
+						{
+							main_step(index, i, xy);
+						}
+					}
+					
+					if (nabla_main == 0.0) {
+						index = index/iter_main_full;
+					}
+					else {
+						float index_pre = index/iter_main_full;
+	
+						main_step(index, i, xy);
+	
+						index = index/(iter_main_full + ${sequence.length}.0); // in depend of how long the sequence is !
+						index = index*nabla_main + index_pre*(1.0-nabla_main);
+					}
+	
+					return vec4( index*( (index > 0.0) ? uColpos-uColmid : uColmid-uColneg   )+uColmid, 1.0 );
+				}			
+			'
+			, false // inject uTime
+			, defaultParams.uniforms.concat( [for (v in formulaParams) v.uniform] )
+			);
+	
 	}
 	
 }
--- src/Main.hx
+++ src/Main.hx
@@ -23,6 +23,9 @@ class Main extends Application
 	var formulaParams = new FormulaParams();
 	var oldUsedParams = new FormulaParams();
 	
+	var formula:Formula;
+	var sequence:Array<String>;
+
 	override function onWindowCreate():Void
 	{
 		switch (window.context.type) {
@@ -48,9 +51,14 @@ class Main extends Application
 		};
 		
 		formulaParams = [
-			"a" => new Param( "a:"   , "uParama", 0, -1,  1 ),
-			"b" => new Param( "b:"   , "uParamb", 0, -1,  1 )
+			"a" => new Param( "a:" , "uParama", 2.5, 0.0, 10 ),
+			"b" => new Param( "b:" , "uParamb", 2.0, 0.0, 10 )
 		];
+
+		formula = "a*sin(i+n)^2+b";
+		for (p in formulaParams.keys()) formula.bind( ("uParam"+p : Formula), p);
+		
+		sequence = ["x", "y"];
 
 		ui = new Ui(peoteView, defaultParams, formulaParams, "a*sin(i+n)^2+b", "xy", onUIInit);
 		
@@ -60,35 +68,74 @@ class Main extends Application
 	{
 		trace("onUiInit");
 
+		Lyapunow.init(lyapunowDisplay, formula, sequence, defaultParams, formulaParams);
 
-		Lyapunow.init(lyapunowDisplay, defaultParams);
-
-		// init uniforms
-		// linesize = new UniformFloat("linesize", 0.1);
+		// var timer = new haxe.Timer(1000); timer.run = updateTime;
 	}	
 
 	// ------------------------------------------------------------
 	// ----------------- LIME EVENTS ------------------------------
 	// ------------------------------------------------------------	
 
-	var formula:Formula;
-	var sequence:String;
-
+	// function updateTime():Void 
 	override function update(deltaTime:Int):Void 
 	{
-		// TODO: --------- check a seqence-change ---------
-		if (Ui.sequenceChanged) {
+		var updateShader = false;
+
+		// --------- check a seqence-change ---------
+
+		if (Ui.sequenceChanged) 
+		{
 			Ui.sequenceChanged = false;
 
 			trace("-------- Sequence change --------:");
-			// TODO: check that it allways have one "x" and one "y" inside
-			// -> have to create also new ui-widgets for more (e.g. "z") 
+
+			// check for removed parameters
+			for (s in sequence) {
+				if ( Ui.sequence.indexOf(s) < 0  &&  formula.hasParam(s) == false ) {
+					trace('remove sequence param "$s"');
+					oldUsedParams.set(s, formulaParams.get(s)); // store it for later usage
+					formulaParams.remove(s);
+					ui.removeFormulaParam(s); // remove that widget by UI
+				}
+			}
+
+
+			var found_x = false;
+			var found_y = false;
+			sequence = [];
+
+			// check that it allways have one "x" and one "y" inside
+			for (i in 0...Ui.sequence.length) {
+				var c = Ui.sequence.charAt(i);
+				if (c == "x") found_x = true;
+				else if (c == "y") found_y = true;
+				else if ( ! formulaParams.exists(c)) {
+					var param:Param = (oldUsedParams.exists(c)) ? oldUsedParams.get(c) : new Param(c, "uParam"+c, 0.0, -10, 10);
+					formulaParams.set( c, param );
+
+					// add new widget by UI !
+					ui.addFormulaParam(c, param);
+				}					
+				sequence.push(c);
+			}
+
+			if (found_x && found_y) {
+				updateShader = true;
+			}
+			else {
+				if (!found_x) trace('ERROR, formula is need parameter "x"');
+				if (!found_y) trace('ERROR, formula is need parameter "y"');
+				// TODO: give error-feedback by UI !
+			}
 		}
 
 
-		var f:Formula = null;
 		// --------- check a formula-change -----------
-		if (Ui.formulaChanged) {
+
+		var f:Formula = null;
+		if (Ui.formulaChanged) 
+		{
 			Ui.formulaChanged = false;
 			
 			trace("-------- Formula change -----------:");
@@ -113,7 +160,7 @@ class Main extends Application
 
 				// check for removed parameters inside formula
 				for (p in formulaParams.keys()) {
-					if (params.indexOf(p) < 0) {
+					if (params.indexOf(p) < 0  &&  sequence.indexOf(p) < 0) {
 						trace('remove param "$p"');
 						oldUsedParams.set(p, formulaParams.get(p)); // store it for later usage
 						formulaParams.remove(p);
@@ -125,7 +172,8 @@ class Main extends Application
 				for (p in params) {
 					if (p == "i") found_i = true;
 					else if (p == "n") found_n = true;
-					else {
+					else if (p != "x" && p != "y")
+					{
 						if ( ! formulaParams.exists(p)) {
 							if (p.length > 8) {
 								trace('ERROR, parameter "$p" should have not more then 8 chars');
@@ -139,34 +187,29 @@ class Main extends Application
 
 							// add new widget by UI !
 							ui.addFormulaParam(p, param);
-
-							// change parameter identifier to have unique name for glsl
-							f.bind( ("uParam"+p : Formula), p);
 						}
+						// change parameter identifier to have unique name for glsl
+						f.bind( ("uParam"+p : Formula), p);
 					}
 				}
 
 				if (found_i && found_n && param_length_ok) {
-
 					formula = f;
-
-					trace("formula ready to update:");
-					trace(f.toString("glsl") , Ui.sequence);
-					trace('sequence: ${Ui.sequence}\n');
-
-					// TODO: -------> call lyapunows update function
-					
+					updateShader = true;
 				}
 				else {
 					if (!found_i) trace('ERROR, formula is need parameter "i"');
 					if (!found_n) trace('ERROR, formula is need parameter "n"');
 					// TODO: give error-feedback by UI !
 				}
-
-
 			}
-
 		}
+
+		if (updateShader) {
+			// call lyapunows update function
+			Lyapunow.updateShader(formula, sequence, defaultParams, formulaParams);
+		}
+
 	}
 
 	// override function onMouseMove (x:Float, y:Float):Void {}
